dfc7bc0ff13ab61c7459f13f8f4f80fe3cfd9b4f
Kamon <90624838+Kamoratraks@users.noreply.github.com>
2026-04-17 16:47:16 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FlashCards from './FlashCards.jsx' // ดึงโค้ด Flashcard มาแสดงผล
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FlashCards />
+  </React.StrictMode>,
+)
